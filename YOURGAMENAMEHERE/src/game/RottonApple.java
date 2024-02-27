846afde246f808a4913323f24984bea58cf5654a
ChenAndy-7 <107810278+ChenAndy-7@users.noreply.github.com>
2024-02-26 23:02:00 -0500

    Update RottonApple.java

--- YOURGAMENAMEHERE/src/game/RottonApple.java
+++ YOURGAMENAMEHERE/src/game/RottonApple.java
@@ -1,9 +1,16 @@
 public class RottonApple extends Snake{
   Color color;
   int timer;
+  int count;
 
-  public RottonApple(int timer) {
+  public RottonApple(int timer, int count) {
     this.color = Color.black;
     this.timer = timer;
+    this.count = count;
+  }
+
+  public void randomSpawn() {
+
+  }
     
 }
