aecc29f02ed2a40773bf3c492ba6e866f1c6175e
ChenAndy-7 <107810278+ChenAndy-7@users.noreply.github.com>
2024-02-26 23:00:50 -0500

    Update Apple.java

--- YOURGAMENAMEHERE/src/game/Apple.java
+++ YOURGAMENAMEHERE/src/game/Apple.java
@@ -1,3 +1,13 @@
-public class Apple{
+public class Apple implements ApplesInt {
+  int count;
+  Color color;
+
+  public Apple(int count) {
+    this.count = count;
+    this.color = Color.Red;
+  }
+
+  public void randomSpawn() {
+    
 }
   
