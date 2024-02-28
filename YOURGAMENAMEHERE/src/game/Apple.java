ec603143d27cb1bb73fccbbac6f053855348d6a3
andy.ch3n <andy.ch3n@wireless-10-104-247-209.umd.edu>
2024-02-28 15:42:41 -0500

    commit3

--- YOURGAMENAMEHERE/src/game/Apple.java
+++ YOURGAMENAMEHERE/src/game/Apple.java
@@ -1,19 +1,45 @@
 package game;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
 
 
-public class Apple implements ApplesInt {
+
+public class Apple extends Polygon implements ApplesInt {
   int count;
   Color color;
+  Random random = new Random();
 
-  public Apple(int count) {
-    this.count = count;
-    this.color = Color.RED;
+  public Apple(Point[] inShape, Point inPosition, double inRotation, int count) {
+	  super(inShape, inPosition, inRotation);
+	  this.count = count;
+	  this.color = Color.RED;
   }
 
   public void randomSpawn() {
+	
   
   }
+  /**
+   * 
+   * @param brush
+   * get the points of the apple object from the inShape array and uses it to 
+   * paint a apple on the board
+   */
+  private void paint(Graphics brush) {
+	Point[] apple = getPoints();
+	
+	int[] xCoord = new int[apple.length];
+	int[] yCoord = new int[apple.length];
+	
+	for(int i = 0; i < apple.length; i++) {
+		xCoord[i] =  (int) apple[i].getX();
+		yCoord[i] = (int) apple[i].getY();
+	}
+	
+	brush.fillPolygon(xCoord, yCoord, apple.length);
+  }
+  
     
 }
   
--- YOURGAMENAMEHERE/src/game/RottonApple.java
+++ YOURGAMENAMEHERE/src/game/RottonApple.java
@@ -1,19 +1,42 @@
 package game;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
 
-public class RottonApple extends Snake implements ApplesInt{
-  Color color;
-  int timer;
-  int count;
+public class RottonApple extends Polygon implements ApplesInt{
+	 Random random = new Random();
+	 Color color;
+	 int timer;
+	 int count;
 
-  public RottonApple(int timer, int count) {
-    this.color = Color.black;
-    this.timer = timer;
-    this.count = count;
+  public RottonApple(Point[] inShape, Point inPosition, double inRotation, int timer, int count) {
+	  super(inShape, inPosition, inRotation);
+	  this.color = Color.black;
+	  this.timer = timer;
+	  this.count = count;
   }
 
   public void randomSpawn() {
-
+	  
   }
+  /**
+   * 
+   * @param brush
+   * get the points of the apple object from the inShape array and uses it to 
+   * paint a apple on the board
+   */
+  private void paint(Graphics brush) {
+		Point[] rottenApple = getPoints();
+		
+		int[] xCoord = new int[rottenApple.length];
+		int[] yCoord = new int[rottenApple.length];
+		
+		for(int i = 0; i < rottenApple.length; i++) {
+			xCoord[i] =  (int) rottenApple[i].getX();
+			yCoord[i] = (int) rottenApple[i].getY();
+		}
+		
+		brush.fillPolygon(xCoord, yCoord, rottenApple.length);
+	  }
     
 }
